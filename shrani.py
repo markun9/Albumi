3326fb764e1d4f96349ca089eb7ede9ac9011a1f
markun9 <jure.markun@gmail.com>
2016-05-09 21:35:38 +0200

    shrani podatke v csv

--- shrani.py
+++ shrani.py
@@ -54,7 +54,7 @@ zanri=['Alternative','Avantgarde','Industrial','Black','Death','Doom','Post','Pr
 for zanr in zanri:
     shrani('http://www.metalstorm.net/bands/albums_top.php?album_style=' + zanr, zanr + '.html')
 
-slovar = {'vrstni_red':[],'bendi':[],'zanri':[], 'albumi':[], 'ocene':[], 'st_glasov':[]}   
+slovar = {'vrstni_red':[],'albumi':[],'leta':[],'avtorji':[],'podzvrsti':[], 'ocene':[], 'st_glasov':[]}   
 for zanr in zanri:
     with open(zanr + '.html','r',encoding='utf-8') as v:
          print("pridem do sm", v)
@@ -62,28 +62,46 @@ for zanr in zanri:
          for line in v:
              bend = re.findall('''<b><a href=/bands/band.php\?band_id=\d+&\w+=[-#&;ÿöä:ô()ÖăåáA-Za-z0-9' _%.,!?"'/$\t\s\+\w]*>([-#&;ÿöä:ô()Ö'ă%åáA-Za-z0-9 _.,!?"'/$\t\s\+\w-]*)</a></b>''', line, flags=re.DOTALL)
              po_vrsti = re.findall('''<span class=dark>(\d+\.)</span>''', line, flags=re.DOTALL)
-             album = re.findall('''<a href=/bands/album.php\?album_id=\d+>([-\[\]#&;ÿöä:ô()ÖăåáA-Za-z0-9' _%.,!?"'/$\t\s\+\w]*)</a> <span class=dark>(\(\d+\))</span></td>''', line, flags=re.DOTALL)
+             album = re.findall('''<a href=/bands/album.php\?album_id=\d+>([-\[\]#&;ÿöä:ô()ÖăåáA-Za-z0-9' _%.,!?"'/$\t\s\+\w]*)</a> <span class=dark>\(\d+\)</span></td>''', line, flags=re.DOTALL)
+             leto = re.findall('''<a href=/bands/album.php\?album_id=\d+>[-\[\]#&;ÿöä:ô()ÖăåáA-Za-z0-9' _%.,!?"'/$\t\s\+\w]*</a> <span class=dark>(\(\d+\))</span></td>''', line, flags=re.DOTALL)
              ocena = re.findall('''<a href=/bands/rating.php\?album_id=\d+>(\d+\.?\d+)</a>''', line, flags=re.DOTALL)
              st_glasov = re.findall('''<span class=dark>\| (\d+)</span>''', line, flags=re.DOTALL)
              if po_vrsti!= []:
                  slovar['vrstni_red'].append(po_vrsti)
              if bend != []:
-                 slovar['bendi'].append(bend)
-                 slovar['zanri'].append([zanr])
+                 slovar['avtorji'].append(bend)
+                 slovar['podzvrsti'].append(zanr)
              if album != []:
                  slovar['albumi'].append(album)
              if ocena != []:
                  slovar['ocene'].append(ocena)
              if st_glasov != []:
                  slovar['st_glasov'].append(st_glasov)
-print("bendov imam" + str(len(slovar['bendi'])))
-print("zanrov imam" + str(len(slovar['zanri'])))
+             if leto != []:
+                 slovar['leta'].append(leto)
+print("bendov imam" + str(len(slovar['avtorji'])))
+print("zanrov imam" + str(len(slovar['podzvrsti'])))
 print("cifr je" + str(len(slovar['vrstni_red'])))
 print("albumov je" + str(len(slovar['albumi'])))
+print("let je" + str(len(slovar['leta'])))
 print("ocena je" + str(len(slovar['ocene'])))
 print("glasov glasov je" + str(len(slovar['st_glasov'])))
 
-print("torej, nek random album je", slovar['bendi'][544], slovar['zanri'][544],
-      slovar['albumi'][544], slovar['ocene'][544], slovar['vrstni_red'][544])
+print("torej, nek random album je", slovar['avtorji'][544], slovar['podzvrsti'][544],
+      slovar['albumi'][544], slovar['ocene'][544], slovar['vrstni_red'][544], slovar['leta'][544])
 
 
+with open('Podatki.csv','w+') as csvfile:
+    oznake = ['vrstni_red', 'albumi','leta','avtorji', 'podzvrsti', 'ocene', 'st_glasov']
+    print(csv)
+    napisi = csv.DictWriter(csvfile, fieldnames=oznake)
+    napisi.writeheader()
+    for i in range(954):
+        napisi.writerow({'vrstni_red': str((slovar['vrstni_red'][i])[0]),
+                         'albumi': str((slovar['albumi'][i])[0]),
+                         'avtorji' : str((slovar['avtorji'][i])[0]),
+                         'leta' : str((slovar['leta'][i])[0]),
+                         'podzvrsti' : (slovar['podzvrsti'][i]),
+                         'ocene' : str((slovar['ocene'][i])[0]),
+                         'st_glasov' : str((slovar['st_glasov'][i])[0])})
+    
